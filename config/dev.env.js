9bc67098d2681ac465236813af8f9acb7f659082
xiaoping <edwardhjp@gmail.com>
2017-08-28 17:56:58 +0800

    Updates

--- config/dev.env.js
+++
@@ -1,8 +0,0 @@
-var merge = require('webpack-merge')
-var prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  BASE_URL: '"http://apitest.example.com"',
-  HTML_TITLE: 'vue-pop-colorpicker - dev'
-})
--- config/prod.env.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  BASE_URL: '"http://api.example.com"',
-  HTML_TITLE: 'vue-pop-colorpicker'
-}
